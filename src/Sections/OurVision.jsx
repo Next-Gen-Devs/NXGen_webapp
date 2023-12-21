c427074467eda384dc1e9bdfea0b79a457965771
Omer Aqeel <macbookpro@omer-aqeel.home>
2023-12-21 06:18:24 +0000

    Added few info about us

--- src/Sections/OurVision.jsx
+++ src/Sections/OurVision.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './OurVision.css';
 
+
 function OurVision() {
 
     return (
         <div className="ourVisionContainer" id='section1'>
-            <div className='titleContainer'>
-                <h1 className='visionTitle'>Our <span className='visionText'>Vision</span></h1>
-            </div>
-            <div className='ourVision-body'>
-                <p>Your vision is our vision Were here to   make your vision into a digital reality be it a e-commerce website, a news portal or a gaming site, you name it we can make it happen. Ready to get started?</p>
-            </div>
+          <div className='titleContainer'>
+            <h1 className='visionTitle'>Who We Are</h1>
+          </div>
+          <div className='ourVision-body'>
+            <div></div>
+            <p>We are a dedicated team committed to turning your vision into digital reality. Whether it's an e-commerce website, a news portal, or a gaming site, we have the expertise to bring your ideas to life. Our mission is to make your digital dreams a tangible and successful reality. Are you ready to get started on your journey with us? <br /> <br />
+              <div className='horizontalLine'>
+                <hr className='centeredHr' />
+              </div>
+            </p>
+          </div>
         </div>
-    )
+      );
+      
 }
 
 export default OurVision;
